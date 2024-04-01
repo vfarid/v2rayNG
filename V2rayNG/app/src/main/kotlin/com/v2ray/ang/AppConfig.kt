097ec5468341abb7f107be672808904be86bd64d
Vahid Farid <vahidfarid@gmail.com>
2024-04-01 15:52:10 +0330

    Update AppConfig.kt

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
@@ -72,9 +72,9 @@ object AppConfig {
         "https://raw.githubusercontent.com/2dust/androidpackagenamelist/master/proxy.txt"
     const val v2rayCustomRoutingListUrl =
         "https://raw.githubusercontent.com/2dust/v2rayCustomRoutingList/master/"
-    const val v2rayNGIssues = "https://github.com/2dust/v2rayNG/issues"
-    const val v2rayNGWikiMode = "https://github.com/2dust/v2rayNG/wiki/Mode"
-    const val v2rayNGPrivacyPolicy = "https://raw.githubusercontent.com/2dust/v2rayNG/master/CR.md"
+    const val v2rayNGIssues = "https://github.com/vfarid/v2rayNGPlus/issues"
+    const val v2rayNGWikiMode = "https://github.com/vfarid/v2rayNGPlus/wiki/Mode"
+    const val v2rayNGPrivacyPolicy = "https://raw.githubusercontent.com/vfarid/v2rayNGPlus/master/CR.md"
     const val promotionUrl = "aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw="
     const val geoUrl = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/"
 
